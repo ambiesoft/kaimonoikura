87cba3ed4994f127a961f54d4e93a1c149dbf32a
ambiesoft <ambiesoft.trueff@gmail.com>
2023-07-14 19:29:39 +0900

    zenkaku % can be used in the Calculator

--- src/Calculator.vue
+++ src/Calculator.vue
@@ -33,6 +33,8 @@ function formatForEval(s) {
     s = s.replace(/＋/g, '+');
     s = s.replace(/－/g, '-');
     s = s.replace(/⁻/g, '-');
+    s = s.replace(/％/g, '%');
+
     s = s.replace(/　/g, ' ');
 
     s = s.replace(/\[/g, '(');
